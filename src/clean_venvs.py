eb597f869186414f2eade5f69bf5f148690099a3
Patrick <patrickgao2020@gmail.com>
2026-08-24 17:26:45 -0400

    Write code to clean up Python virtual environments.

---
+++ src/clean_venvs.py
@@ -0,0 +1,74 @@
+import os
+import pathlib
+import re
+import subprocess
+import sys
+
+yes_pattern = re.compile(r"(?i)\b(y|yes|yeah|yep|yup|sure|ok(?:ay)?|alright|affirmative|correct|true|indeed|absolutely|definitely|of course|sounds good|why not|i think so|i guess so)\b")
+no_pattern = re.compile(r"(?i)\b(n|no|nope|nah|naw|negative|never|false|incorrect|not really|not at all|absolutely not|do not|don't|cannot|can't|won't)\b")
+
+def read_yes_no(prompt):
+    while True:
+        response = input(prompt)
+        if yes_pattern.search(response) and not no_pattern.search(response):
+            return True
+        elif no_pattern.search(response) and not yes_pattern.search(response):
+            return False
+        else:
+            print("I didn't understand that.")
+
+def get_subdirectories(path):
+	return [pathlib.Path(root) / directory for root, directories, files in os.walk(path) for directory in directories]
+
+def find_python(path):
+	possible_pythons = [path / "bin" / "python", path / "Scripts" / "python.exe"]
+	for possible_python in possible_pythons:
+		if possible_python.is_file():
+			if subprocess.run([str(possible_python), "-c", "import sys; print(sys.prefix == sys.base_prefix)"], capture_output=True, text=True).stdout.strip() == "False":
+				return possible_python
+	return None
+
+def process_directory(path):
+	if not (path / "pyvenv.cfg").is_file():
+		return
+	python = find_python(path)
+	if not python:
+		return
+	confirmation = read_yes_no(f"Removing virtual environment at {path}. Would you like to continue? ")
+	if not confirmation:
+		print(f"Skipping virtual environment at {path}.")
+		return
+	try:
+		requirements = subprocess.run([str(python), "-m", "pip", "freeze"], check=True, timeout=60, capture_output=True, text=True)
+	except subprocess.CalledProcessError:
+		print(f"Failed to run pip freeze on virtual environment at {path}.")
+		print(f"Error code: {requirements.returncode}")
+		print(f"Error output: {requirements.stderr}")
+		return
+	except subprocess.TimeoutExpired:
+		print(f"Running pip freeze on virtual environment at {path} took too long.")
+		return
+	requirements_file = path.parent / "requirements.txt"
+	with open(requirements_file, "w") as fout:
+		fout.write(requirements.stdout)
+	subprocess.run(["rm", "-rf", path])
+	print(f"Sucessfully wrote requirements.txt file and deleted virtual environment at {path}.")
+	return
+
+def clean_up(root):
+	directories = get_subdirectories(root)
+	for directory in directories:
+		if directory.is_dir():
+			process_directory(directory)
+
+if __name__ == "__main__":
+	if len(sys.argv) > 1:
+		for i in range(1, len(sys.argv)):
+			try:
+				root = pathlib.Path(sys.argv[i]).resolve(strict=True)
+			except FileNotFoundError:
+				print(f"Could not resolve {sys.argv[i]}. Continuing to the next argument...")
+				continue
+			clean_up(root)
+	else:
+		clean_up(pathlib.Path.cwd())
